1b5b14035c91058699d3bdc00e1a8786b29387c9
Gouranga <gourangasaha1206@gmail.com>
2023-03-04 20:28:19 +0530

    easily toggle between simple to scientific calculator

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
+function expand(check){
+    if(check===true){
+document.getElementById("Calculator_body").style.width="45vw";
+document.getElementsByClassName("buttons")[0].style=[
+"width: 40vw;",
+];
+document.getElementById("scientific_div").style.display="block";
+document.getElementById("tu").innerHTML= "<button class=\"btn\">x<sup>y</sup></button>";
+    }
+    else {
+    document.getElementById("Calculator_body").style.width="20%";
+    document.getElementsByClassName("buttons")[0].style=["width: 20vw;"];
+    document.getElementById("scientific_div").style.display="none";
+    document.getElementById("tu").innerHTML= "<button class=\"btn\" onclick=expand(true)>&#8689</button>";
 
-let display= document.getElementsByClassName("display_input");
+};
+}
