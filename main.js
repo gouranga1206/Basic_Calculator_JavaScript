9990da541d41a2d1c26bb44e225c4502acd97a09
Gouranga <gourangasaha1206@gmail.com>
2023-03-05 09:17:11 +0530

    Modifying for Scientific Calculator

--- main.js
+++ main.js
@@ -15,3 +15,31 @@ document.getElementById("tu").innerHTML= "<button class=\"btn\">x<sup>y</sup></b
 
 };
 }
+
+let string="";
+let buttons = document.querySelectorAll(".btn");
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if(e.target.innerHTML=="="){
+            string = eval(string);
+            document.querySelector(".display_input").value = string;
+        }
+        else if(e.target.innerHTML=="AC"){
+            string="";
+            document.querySelector(".display_input").value = string;
+        }
+        else if(e.target.id =="back"){
+            string = string.slice(0,-1);
+            document.querySelector(".display_input").value = string;
+        }
+        // else if(){
+
+        // }
+        else{
+        string += e.target.innerHTML;
+        document.querySelector(".display_input").value = string;
+        }
+}
+    
+    );
+});
