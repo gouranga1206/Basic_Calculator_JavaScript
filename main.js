0d30dc1adc3c15602b8656e87d2aafbf5ef98abc
Gouranga <gourangasaha1206@gmail.com>
2023-03-05 10:12:24 +0530

    dropped Scientific calc

--- main.js
+++ main.js
@@ -1,45 +1,24 @@
-function expand(check){
-    if(check===true){
-document.getElementById("Calculator_body").style.width="45vw";
-document.getElementsByClassName("buttons")[0].style=[
-"width: 40vw;",
-];
-document.getElementById("scientific_div").style.display="block";
-document.getElementById("tu").innerHTML= "<button class=\"btn\">x<sup>y</sup></button>";
-    }
-    else {
-    document.getElementById("Calculator_body").style.width="20%";
-    document.getElementsByClassName("buttons")[0].style=["width: 20vw;"];
-    document.getElementById("scientific_div").style.display="none";
-    document.getElementById("tu").innerHTML= "<button class=\"btn\" onclick=expand(true)>&#8689</button>";
 
-};
-}
 
-let string="";
+let string = "";
 let buttons = document.querySelectorAll(".btn");
 Array.from(buttons).forEach((button) => {
-    button.addEventListener("click", (e) => {
-        if(e.target.innerHTML=="="){
-            string = eval(string);
-            document.querySelector(".display_input").value = string;
-        }
-        else if(e.target.innerHTML=="AC"){
-            string="";
-            document.querySelector(".display_input").value = string;
-        }
-        else if(e.target.id =="back"){
-            string = string.slice(0,-1);
-            document.querySelector(".display_input").value = string;
-        }
-        // else if(){
-
-        // }
-        else{
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      document.querySelector(".display_input").value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      document.querySelector(".display_input").value = string;
+    } else if (e.target.id == "back") {
+      string = string.slice(0, -1);
+      document.querySelector(".display_input").value = string;
+    }
+      else{
         string += e.target.innerHTML;
         document.querySelector(".display_input").value = string;
-        }
-}
-    
+      }
+    }
     );
+
 });
